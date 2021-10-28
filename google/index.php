12e2051a97f7b2f2cf02978a8e857b1eb7771f80
jmanning58 <manningjc@g.cofc.edu>
2021-10-27 20:16:25 -0400

    Taken out personal authentication token

--- google/index.php
+++ google/index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <script src = "js/googlemaps.js"></script>
-    <style type = "text/css">
-        .container {
-            height: 450px;
-        }
-        #map {
-            width: 100%;
-            height: 100%;
-        }
-    </style>
-</head>
+    <head>
+        <script src = "js/googlemaps.js"></script>
+        <style type = "text/css">
+            .container {
+                height: 450px;
+            }
+            #map {
+               width: 100%;
+               height: 100%;
+            }
+        </style>
+    </head>
 <body>
     <div class = "container">
         <div id = "map"></div>
     </div>
 </body>
 <script async defer 
-    src = "https://maps.googleapis.com/maps/api/
-    js?key=AIzaSyA13Szq87GpErV5A_-FlEjhFALINp-E9_E&callback=loadMap">
+    src = "https://maps.googleapis.com/maps/api/js?key=YOUR_AUTH_KEY&callback=loadMap">
 </script>
 </html>
--- google/js/googlemaps.js
+++ google/js/googlemaps.js
@@ -1,3 +1,5 @@
+let map;
+
 function loadMap() {
     var uluru = {lat: -23.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
