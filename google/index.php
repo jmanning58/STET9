1a6115b8f2456dce88b450c5177945958ed7fdcb
jmanning58 <manningjc@g.cofc.edu>
2021-11-01 15:07:53 -0400

    Add the google marker

--- google/index.php
+++ google/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src = "js/googlemaps.js"></script>
+        <script type = "text/javascript" src = "js/googlemaps.js"></script>
         <style type = "text/css">
             .container {
                 height: 450px;
--- google/js/googlemaps.js
+++ google/js/googlemaps.js
@@ -3,7 +3,12 @@ let map;
 function loadMap() {
     var uluru = {lat: -23.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
+        zoom: 8,
         center: uluru
+    });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
     });
 }
